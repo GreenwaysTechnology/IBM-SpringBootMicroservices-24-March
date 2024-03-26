d30fc6aaba496afb0c2365a3e5e7f2c1305f11c1
subugee <sasubramanian_md@hotmail.com>
2024-03-26 13:20:07 +0530

    spring data

--- restapp/src/main/java/com/ibm/web/api/todos/service/TodoService.java
+++ restapp/src/main/java/com/ibm/web/api/todos/service/TodoService.java
@@ -25,15 +25,32 @@ public class TodoService {
     @Bean
     public CommandLineRunner init() {
         return args -> {
-        todos = List.of(
-                new Todo(1l, 1l, "delectus aut autem", false),
-                new Todo(1l, 2l, "quis ut nam facilis et officia qui", false),
-                new Todo(1l, 3l, "fugiat veniam minus", false),
-                new Todo(1l, 4l, "et porro tempora", true));
+            todos = List.of(
+                    new Todo(1l, 1l, "delectus aut autem", false),
+                    new Todo(1l, 2l, "quis ut nam facilis et officia qui", false),
+                    new Todo(1l, 3l, "fugiat veniam minus", false),
+                    new Todo(1l, 4l, "et porro tempora", true));
         };
     }
 
     public List<Todo> findAll() {
         return todos;
+    }
+
+    public Todo findById(Long item) {
+        return new Todo(item, 8l, "Learn Spring", false);
+    }
+
+    public List<Todo> filterBy(Boolean status) {
+        return List.of(new Todo(1l, 1l, "delectus aut autem", status));
+    }
+
+    public Todo save(Todo todo) {
+        //YOu have to write logic to insert
+        return todo;
+    }
+
+    public Todo update(Todo todo, Long id) {
+        return new Todo(todo.getUserId(), todo.getId(), todo.getTitle(), todo.isCompleted());
     }
 }
